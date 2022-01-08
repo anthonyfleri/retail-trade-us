a6c492e43d4356768cfdae6b154f4bbb0063912d
anthonyfleri <64998923+anthonyfleri@users.noreply.github.com>
2022-01-08 10:11:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,33 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-import pandas as pd
 import plotly.express as px
+import pandas as pd
 
-# call the data
-retail = pd.read_csv('https://raw.githubusercontent.com/anthonyfleri/retail-trade-us/master/retail_trade.csv', dtype = {'Year':object})
-retail
-
-
-# initialize the app
 app = dash.Dash(__name__)
-server = app.server
 
-# define and set the layout
-year_options = retail['Year'].unique()
-category_options = retail['Category'].unique()
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
 
-app.layout = html.Div([
-    html.Div([
-        
-        html.H1('Retail Trade - US', style={'text-align':'left','font-family':'open sans light'}),
-    
-        html.Div([
-            dcc.Dropdown(
-                id='year-filter',
-                options=[{'label': i, 'value': i} for i in year_options],
-                value='2021')
-                ], style={'width': '48%', 'display': 'inline-block'}),
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-        html.Div([
-            dcc.Dropdown(
-                id='category-filter',
-                options=[{'label': i, 'value': i} for i in category_options],
-                value='Clothing/Fashion')
-                ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
-            ]),
-    
-            dcc.Graph(id='retail-graphic')
-    
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
 ])
 
-@app.callback(
-    Output('retail-graphic', 'figure'),
-    Input('year-filter', 'value'),
-    Input('category-filter','value'))
-def update_figure(selected_year,
-                  selected_category):
-    
-    filtered_df = retail[(retail['Year'] == selected_year)  & (retail['Category'] == selected_category)]
-    
-    fig = px.bar(filtered_df, x='Retail Type', y='Sales', 
-                 barmode='group', height = 500)
-
-    fig.update_layout(xaxis_tickangle=45, showlegend=False, xaxis={'categoryorder':'total descending'})
-    fig.update_xaxes(tickfont=dict(size=8))
-
-    return fig
-
 if __name__ == '__main__':
-    app.run_server(debug=True, use_reloader=False)
-
+    app.run_server(debug=True)
