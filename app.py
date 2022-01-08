71eaa0a51a0e1e6104025c0d61f3efd0473e1083
anthonyfleri <64998923+anthonyfleri@users.noreply.github.com>
2022-01-08 11:58:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,42 +2,62 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import pandas as pd
 import plotly.express as px
 
-import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
-
+# initialize the app
 app = dash.Dash(__name__)
 server = app.server
 
+# call the data
+retail = pd.read_csv('https://raw.githubusercontent.com/anthonyfleri/retail-trade-us/master/retail_trade.csv', dtype = {'Year':object})
+
+
+# define and set the layout
+year_options = retail['Year'].unique()
+category_options = retail['Category'].unique()
+
 app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        id='year-slider',
-        min=df['year'].min(),
-        max=df['year'].max(),
-        value=df['year'].min(),
-        marks={str(year): str(year) for year in df['year'].unique()},
-        step=None
-    )
+    html.Div([
+        
+        html.H1('Retail Trade - US', style={'text-align':'left','font-family':'open sans light'}),
+    
+        html.Div([
+            dcc.Dropdown(
+                id='year-filter',
+                options=[{'label': i, 'value': i} for i in year_options],
+                value='2021')
+                ], style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='category-filter',
+                options=[{'label': i, 'value': i} for i in category_options],
+                value='Clothing/Fashion')
+                ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+            ]),
+    
+            dcc.Graph(id='retail-graphic')
+    
 ])
 
-
 @app.callback(
-    Output('graph-with-slider', 'figure'),
-    Input('year-slider', 'value'))
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
+    Output('retail-graphic', 'figure'),
+    Input('year-filter', 'value'),
+    Input('category-filter','value'))
+def update_figure(selected_year,
+                  selected_category):
+    
+    filtered_df = retail[(retail['Year'] == selected_year)  & (retail['Category'] == selected_category)]
+    
+    fig = px.bar(filtered_df, x='Retail Type', y='Sales', 
+                 barmode='group', height = 500)
 
-    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
-                     size="pop", color="continent", hover_name="country",
-                     log_x=True, size_max=55)
-
-    fig.update_layout(transition_duration=500)
+    fig.update_layout(xaxis_tickangle=45, showlegend=False, xaxis={'categoryorder':'total descending'})
+    fig.update_xaxes(tickfont=dict(size=8))
 
     return fig
 
-
 if __name__ == '__main__':
-    app.run_server)
+    app.run_server(debug=True, use_reloader=False)
