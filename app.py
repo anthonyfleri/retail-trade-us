d5378f0830ff6d42955722390741b95ee1de86e7
anthonyfleri <64998923+anthonyfleri@users.noreply.github.com>
2022-01-08 10:19:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,28 +7,55 @@ import pandas as pd
 app = dash.Dash(__name__)
 server = app.server
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+# call the data
+retail = pd.read_csv('https://raw.githubusercontent.com/anthonyfleri/retail-trade-us/master/retail_trade.csv', dtype = {'Year':object})
+retail
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+# define and set the layout
+year_options = retail['Year'].unique()
+category_options = retail['Category'].unique()
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+app.layout = html.Div([
+    html.Div([
+        
+        html.H1('Retail Trade - US', style={'text-align':'left','font-family':'open sans light'}),
+    
+        html.Div([
+            dcc.Dropdown(
+                id='year-filter',
+                options=[{'label': i, 'value': i} for i in year_options],
+                value='2021')
+                ], style={'width': '48%', 'display': 'inline-block'}),
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+        html.Div([
+            dcc.Dropdown(
+                id='category-filter',
+                options=[{'label': i, 'value': i} for i in category_options],
+                value='Clothing/Fashion')
+                ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+            ]),
+    
+            dcc.Graph(id='retail-graphic')
+    
 ])
+
+@app.callback(
+    Output('retail-graphic', 'figure'),
+    Input('year-filter', 'value'),
+    Input('category-filter','value'))
+def update_figure(selected_year,
+                  selected_category):
+    
+    filtered_df = retail[(retail['Year'] == selected_year)  & (retail['Category'] == selected_category)]
+    
+    fig = px.bar(filtered_df, x='Retail Type', y='Sales', 
+                 barmode='group', height = 500)
+
+    fig.update_layout(xaxis_tickangle=45, showlegend=False, xaxis={'categoryorder':'total descending'})
+    fig.update_xaxes(tickfont=dict(size=8))
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server()
